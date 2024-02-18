62106b10199ca6480a85401a9b43686b05088b35
DESKTOP-SPIP1CH\mhung <abcxyz9cd@gmail.com>
2024-02-19 00:51:26 +0700

    Add tailwind default config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
@@ -7,7 +8,25 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Helvetica, Arial, sans-serif',
+      },
+      colors: {
+        primary: '#ee4d2d',
+        primaryBg: '#fef6f5',
+        primaryGradient: 'linear-gradient(#ee4d2d,#ff7337);',
+        secondaryBlue: '#0046ab',
+        secondaryYellow: '#eda500',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 };
