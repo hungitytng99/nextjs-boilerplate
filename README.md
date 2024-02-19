ca7eb9c884aade791c2e8062c93593d1ed93e3c5
DESKTOP-SPIP1CH\mhung <abcxyz9cd@gmail.com>
2024-02-19 18:58:23 +0700

    Update readme

--- README.md
+++ README.md
@@ -7,8 +7,23 @@
 
 ## Coding conventions
 - Hạn chế dùng let, ưu tiên dùng const, không dùng var
+- Làm việc với ảnh và file:
+```
+1. Tải ảnh lên folder public/images
+2. Khai báo trong @/shared/configs/app.images
+3. Import vào nơi cần sử dụng.
+```
+- Khai báo components được sử dụng trong pages tương ứng với folder trong components 
+- Các component common (form item, button, link,...) thì được khai báo trong @/components/common. Tất cả component trong @/components/common đều phải đặt tên theo qui ước: **App\*\***
+- Các api của hệ thống sẽ được khai báo trong: @/shared/api/**.api.ts
+- Ưu tiên dùng class/property của tailwindcss. Trong trường hợp tailwindcss không hỗ trợ hoặc tốn thời gian implement thì sẽ dùng phương án dễ nhất để implement.
 
 ## More note
+```
+1. Muốn call api thì sử dụng variable: appRequest
+2. Muốn lưu trữ vào bộ nhớ trình duyệt (cookies) thì dùng: appStorage
+3. Log các thông tin quan trọng (optional): appLogger
+```
 
 ## Work flow
 1. Checkout sang branch mới khi làm 1 feature.
@@ -24,5 +39,5 @@ Khi commit cần commit dưới dạng: `[Prefix]:[Description]` với `Prefix` 
 - chore: những sửa đổi nhỏ nhặt không liên quan tới code
 - style: những thay đổi không làm thay đổi ý nghĩa của code như thay đổi css/ui chẳng hạn.
 - perf: code cải tiến về mặt hiệu năng xử lý
-- vendor: cập nhật version cho các dependencies, packages.\
+- vendor: cập nhật version cho các dependencies, packages.
 Sau khi commit cần tạo PR và báo cho Teamleader
--- src/shared/api/user.api.ts
+++ src/shared/api/user.api.ts
@@ -1,14 +1,10 @@
-import { appContainer } from '@/shared/configs';
-import { RequestState } from '@/shared/configs/app.contants';
-import IRequest from '@/shared/interfaces/request/IRequest';
-import ICommonResponse from '@/shared/interfaces/response/ICommonResponse';
-import { ShareSymbol } from '@/shared/interfaces/share.types';
-import { MockApiHelper } from '@/shared/helpers/mock-api.helper';
+import { TodoEntity } from '@/shared/app-model/entities/todo.entity';
 import { TokenEntity } from '@/shared/app-model/entities/token.entity';
 import { NormalLoginBody } from '@/shared/app-model/params/login.param';
-import { TodoEntity } from '@/shared/app-model/entities/todo.entity';
-
-const httpRequest = appContainer.get<IRequest>(ShareSymbol.IRequest);
+import { RequestState } from '@/shared/configs/app.contants';
+import { MockApiHelper } from '@/shared/helpers/mock-api.helper';
+import ICommonResponse from '@/shared/interfaces/response/ICommonResponse';
+import { appRequest } from '@/shared/configs/app.di-container';
 
 export const userRequest = {
   login: async (body?: NormalLoginBody) => {
@@ -39,7 +35,7 @@ export const userRequest = {
   getTodos: async (params?: {}) => {
     try {
       // Api call
-      const todoResponse: Array<TodoEntity> = await httpRequest.get<Array<TodoEntity>>(
+      const todoResponse: Array<TodoEntity> = await appRequest.get<Array<TodoEntity>>(
         'https://jsonplaceholder.typicode.com/todos',
         params,
         { isFullPath: true },
